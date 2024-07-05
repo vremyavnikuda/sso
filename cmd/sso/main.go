956b0cf82e3d54020bd24a361eb347b3320adf00
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-07-05 17:10:33 +0700

    add config log

---
+++ cmd/sso/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	// TODO: инициализировать объект конфига
+	cfg := config.MustLoad()
+
+	//TODO: инициализировать logger
+	log := setupLogger(cfg.Env)
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+	log.Error("error message")
+	log.Warn("warn message")
+
+	//TODO: инициализировать приложение (app)
+
+	//TODO: запустить gRPC сервер
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module sso
+
+go 1.22
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/vremyavnikuda/protos v0.0.7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
